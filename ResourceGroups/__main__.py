bc29738b0515717343854c34a70501ba385af2be
Peter McLarty <turagittech@turagit.com>
2022-04-17 12:40:19 +1000

    Initial Commit
    adding initial files with basic networking and resource groups

--- ResourceGroups/__main__.py
+++ ResourceGroups/__main__.py
@@ -39,24 +39,3 @@ stack_name = auto.fully_qualified_stack_name("turagittech2", project_name, stack
 print(stack_name)
 
 
-
-
-# Create an Azure Resource Group
-resource_group = resources.ResourceGroup('resource_group')
-
-# Create an Azure resource (Storage Account)
-account = storage.StorageAccount('sa',
-    resource_group_name=resource_group.name,
-    sku=storage.SkuArgs(
-        name=storage.SkuName.STANDARD_LRS,
-    ),
-    kind=storage.Kind.STORAGE_V2)
-
-# Export the primary key of the Storage Account
-primary_key = pulumi.Output.all(resource_group.name, account.name) \
-    .apply(lambda args: storage.list_storage_account_keys(
-        resource_group_name=args[0],
-        account_name=args[1]
-    )).apply(lambda accountKeys: accountKeys.keys[0].value)
-
-pulumi.export("primary_storage_key", primary_key)
